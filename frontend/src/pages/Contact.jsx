52c757bb80d9998e559c6d04c33e849764242486
Suchitra-Koduru <suchitrasr5212@gmail.com>
2025-02-04 22:51:11 +0530

    modified contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>Hyderabad, India</p>
           <p className=' text-gray-500'>Tel: +91 98765 43210 <br /> Email: contact@docapp.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT DOCAPP</p>
-          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
+          {/* <p className=' font-semibold text-lg text-gray-600'>CAREERS AT DOCAPP</p> */}
+          {/* <p className=' text-gray-500'>Learn more about our teams and job openings.</p> */}
           {/* <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button> */}
         </div>
       </div>
